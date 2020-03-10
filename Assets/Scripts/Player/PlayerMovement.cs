ebf3999271a46f8c45a479b5a823ee126e952f26
Peter <pjthompson.au@gmail.com>
2020-03-10 17:01:55 +1100

    stuff

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,7 +9,7 @@ using System.Collections;
 [AddComponentMenu("CarSim/Player/Player Movement")]
 public class PlayerMovement : MonoBehaviour
 {
-	#region Variables
+
 	[Header("Player Movement")]
 	[Space(10)]
 	[Header("Character Move Direction")]
@@ -26,63 +26,69 @@ public class PlayerMovement : MonoBehaviour
 	public float walkSpeed, gravity;
 	public float runSpeed;
 	public float airSpeed;
-	public static bool canMove = true;
 
-	#endregion
-	#region Start
+
 	void Start()
 	{
-		canMove = true;
+
 
 		//charc is on this game object we need to get the character controller that is attached to it
 		charCtrl = this.GetComponent<CharacterController>();
 	}
 
-	#endregion
-	#region Update
+
 	void Update()
 	{
-		if (canMove)
+		//if our character is grounded
+		if (charCtrl.isGrounded)
 		{
-			//if our character is grounded
-			if (charCtrl.isGrounded)      // Running
+			//moveDir has the value of Input.Get Axis.. Horizontal, 0, Vertical
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			//moveDir is transformed in the direction of our moveDir
+			moveDirection = transform.TransformDirection(moveDirection);
+			//our moveDir is then multiplied by our speed
+			moveDirection *= walkSpeed;
+
+			//we can also jump if we are grounded so
+			//if the input button for jump is pressed then
+			if (Input.GetButton("Jump"))
 			{
-				//moveDir has the value of Input.Get Axis.. Horizontal, 0, Vertical
-				moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-				//moveDir is transformed in the direction of our moveDir
-				moveDirection = transform.TransformDirection(moveDirection);
-				//our moveDir is then multiplied by our speed
-				moveDirection *= walkSpeed;
-
-				//we can also jump if we are grounded so
-				//if the input button for jump is pressed then
-				if (Input.GetButton("Jump"))
-				{
-					//our moveDir.y is equal to our jump speed
-					moveDirection.y = jumpSpeed;
-				}
-			}
-
-			if (charCtrl.isGrounded && Input.GetKey("left shift"))      // Running
-			{
-				//moveDir has the value of Input.Get Axis.. Horizontal, 0, Vertical
-				moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-				//moveDir is transformed in the direction of our moveDir
-				moveDirection = transform.TransformDirection(moveDirection);
-				//our moveDir is then multiplied by our speed
-				moveDirection *= runSpeed;
-
-				if (Input.GetButton("Jump"))
-				{
-					//our moveDir.y is equal to our jump speed
-					moveDirection.y = jumpSpeed;
-				}
+				//our moveDir.y is equal to our jump speed
+				moveDirection.y = jumpSpeed;
 			}
 		}
+
+		if (charCtrl.isGrounded && Input.GetKey("left shift"))      // Running
+		{
+			//moveDir has the value of Input.Get Axis.. Horizontal, 0, Vertical
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			//moveDir is transformed in the direction of our moveDir
+			moveDirection = transform.TransformDirection(moveDirection);
+			//our moveDir is then multiplied by our speed
+			moveDirection *= runSpeed;
+
+			if (Input.GetButton("Jump"))
+			{
+				//our moveDir.y is equal to our jump speed
+				moveDirection.y = jumpSpeed;
+			}
+		}
+
+		//if our character is grounded
+		if (charCtrl.isGrounded == false)
+		{
+
+			moveDirection.x = Input.GetAxis("Horizontal") / 10;
+			moveDirection.z = Input.GetAxis("Vertical") / 10;
+			moveDirection *= airSpeed;
+
+		}
+
+
 		//regardless of if we are grounded or not the players moveDir.y is always affected by gravity timesed my time.deltaTime to normalize it
 		moveDirection.y -= gravity * Time.deltaTime;
 		//we then tell the character Controller that it is moving in a direction timesed Time.deltaTime
 		charCtrl.Move(moveDirection * Time.deltaTime);
 	}
-	#endregion
+
 }
