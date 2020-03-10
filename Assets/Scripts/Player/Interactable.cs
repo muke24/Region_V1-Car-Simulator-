2ef731270f4bae05cf53b053bf201da33eb5ed1e
Peter <pjthompson.au@gmail.com>
2020-03-10 15:58:27 +1100

    FPS

---
+++ Assets/Scripts/Player/Interactable.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interactable : MonoBehaviour
+{
+	public bool inCar;
+	public float interactDist = 3f;
+
+	public GameObject car;
+	public GameObject player;
+	public Text intText;
+	public Camera carCam;
+
+	public PlayerMovement pm;
+	public MouseLook ml;
+
+	public CarController cc;
+	public InputManager im;
+	
+
+	public List<DragObject> dragObjects;
+	// Start is called before the first frame update
+	void Start()
+	{
+		inCar = false;
+		carCam.gameObject.SetActive(false);
+		player.gameObject.SetActive(true);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+
+		if (interactDist >= Vector3.Distance(car.transform.position, transform.position) && !inCar)
+		{
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+				inCar = true;
+				player.SetActive(false);
+				carCam.gameObject.SetActive(true);
+			}
+			intText.gameObject.SetActive(true);
+		}
+
+		if (interactDist <= Vector3.Distance(car.transform.position, transform.position) && !inCar)
+		{
+			intText.gameObject.SetActive(false);
+		}
+
+	}
+	void OnDrawGizmos()
+	{
+		// Draw a yellow sphere at the transform's position
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(car.transform.position, interactDist);
+	}
+
+	public void InCar()
+	{
+		if (inCar)
+		{
+			pm.enabled = false;
+			ml.enabled = false;
+			im.enabled = true;
+			foreach (DragObject drag in dragObjects)
+			{
+				drag.enabled = true;
+			}
+			
+		}
+		if (!inCar)
+		{
+			pm.enabled = true;
+			ml.enabled = true;
+			im.enabled = false;
+			foreach (DragObject drag in dragObjects)
+			{
+				drag.enabled = false;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//this script can be found in the Component section under the option Character Set Up 
+//Character Movement
+//This script requires the component Character controller
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("CarSim/Player/Player Movement")]
+public class PlayerMovement : MonoBehaviour
+{
+	#region Variables
+	[Header("Player Movement")]
+	[Space(10)]
+	[Header("Character Move Direction")]
+	//vector3 called moveDirection
+	public Vector3 moveDirection;
+
+	//we will use this to apply movement in worldspace
+	private CharacterController charCtrl;
+
+	[Header("Character Variables")]
+
+	//public float variables jumpSpeed, speed, gravity
+	public float jumpSpeed;
+	public float walkSpeed, gravity;
+	public float runSpeed;
+	public float airSpeed;
+	public static bool canMove = true;
+
+	#endregion
+	#region Start
+	void Start()
+	{
+		canMove = true;
+
+		//charc is on this game object we need to get the character controller that is attached to it
+		charCtrl = this.GetComponent<CharacterController>();
+	}
+
+	#endregion
+	#region Update
+	void Update()
+	{
+		if (canMove)
+		{
+			//if our character is grounded
+			if (charCtrl.isGrounded)      // Running
+			{
+				//moveDir has the value of Input.Get Axis.. Horizontal, 0, Vertical
+				moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+				//moveDir is transformed in the direction of our moveDir
+				moveDirection = transform.TransformDirection(moveDirection);
+				//our moveDir is then multiplied by our speed
+				moveDirection *= walkSpeed;
+
+				//we can also jump if we are grounded so
+				//if the input button for jump is pressed then
+				if (Input.GetButton("Jump"))
+				{
+					//our moveDir.y is equal to our jump speed
+					moveDirection.y = jumpSpeed;
+				}
+			}
+
+			if (charCtrl.isGrounded && Input.GetKey("left shift"))      // Running
+			{
+				//moveDir has the value of Input.Get Axis.. Horizontal, 0, Vertical
+				moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+				//moveDir is transformed in the direction of our moveDir
+				moveDirection = transform.TransformDirection(moveDirection);
+				//our moveDir is then multiplied by our speed
+				moveDirection *= runSpeed;
+
+				if (Input.GetButton("Jump"))
+				{
+					//our moveDir.y is equal to our jump speed
+					moveDirection.y = jumpSpeed;
+				}
+			}
+		}
+		//regardless of if we are grounded or not the players moveDir.y is always affected by gravity timesed my time.deltaTime to normalize it
+		moveDirection.y -= gravity * Time.deltaTime;
+		//we then tell the character Controller that it is moving in a direction timesed Time.deltaTime
+		charCtrl.Move(moveDirection * Time.deltaTime);
+	}
+	#endregion
+}
