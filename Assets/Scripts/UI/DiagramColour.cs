1f893c0c116e8d4fd504f6933fa196f5815bd598
Peter <pjthompson.au@gmail.com>
2020-03-15 13:25:19 +1100

    diagramcolour

---
+++ Assets/Scripts/UI/DiagramColour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiagramColour : MonoBehaviour
+{
+	public List<JointLock> jointLocks;
+
+	public List<Image> diagramParts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void Damaged()
+	{
+
+	}
+}
