07d783f5d605d164d166762e04dd7b18a9b00830
Peter <pjthompson.au@gmail.com>
2020-03-18 15:30:38 +1100

    Sniper

---
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+	public Animator playerAnimation;
+
+	public bool getAxisVertical;
+
+	// Update is called once per frame
+	void Update()
+	{
+		/*
+		if (!Input.GetKey(KeyCode.W) || !Input.GetKey(KeyCode.S))
+		{
+			getAxisVertical = false;
+		}
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+		{
+			getAxisVertical = true;
+		}
+		*/
+
+		if (!Input.GetButton("Vertical"))
+		{
+			//playerAnimation.Play("Idle", 1);
+			playerAnimation.SetBool("Moving", false);
+		}
+		if (Input.GetButton("Vertical"))
+		{
+			//playerAnimation.Play("Moving", 1, Input.GetAxis("Vertical"));
+			playerAnimation.SetBool("Moving", true);
+			playerAnimation.SetFloat("MoveSpeed", Input.GetAxis("Vertical"));
+		}
+
+		if (!Input.GetMouseButton(1))
+		{
+			playerAnimation.SetBool("Aim", false);
+			//playerAnimation.Play("SniperIdle", 0);
+		}
+		if (Input.GetMouseButton(1))
+		{
+			playerAnimation.SetBool("Aim", true);
+			//playerAnimation.Play("SniperZoom", 0);
+		}
+
+	}
+}
---
+++ Assets/Scripts/Weapons/Sniper/Sniper.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sniper : MonoBehaviour
+{
+	[Header("Gun Animations")]
+	public Animator animator;
+	public GameObject scopedCam;
+
+
+	[Header("Shoot")]
+
+	public float damage = 100f;
+	public float range = 1000f;
+	public float fireRate = 1f;
+	public float bulletCount = 5f;
+
+	[Space(10)]
+	
+	public ParticleSystem muzzelFlash;
+	public GameObject raycastAim;
+	public GameObject impactEffect;
+
+	public AudioSource gunShot;
+
+	private float nextTimeToFire = 0f;
+
+	[Header("Sensitivity")]
+	float HorizontalSpeed = 2.0f;
+	float VerticalSpeed = 2.0f;
+
+	float scopedHorizontalSpeed = 1.0f;
+	float scopedVerticalSpeed = 1.0f;
+
+	private bool isScoped = false;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
