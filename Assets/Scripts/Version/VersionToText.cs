d955ac4376293f6ee1ca99ce52b63170df44563c
Peter <pjthompson.au@gmail.com>
2020-03-30 04:29:39 +1100

    Version text for game launcher

---
+++ Assets/Scripts/Version/VersionToText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class VersionToText : MonoBehaviour
+{
+	public string version = "1.0";
+	public string path;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+		CreateText();
+	}
+
+	void CreateText()
+	{
+		path = Application.persistentDataPath + "/Version.txt";
+		File.WriteAllText(path, version);
+	}
+
+}
