90a8ac22fb091cbdb66d783e3901ce2accf7a750
Peter <pjthompson.au@gmail.com>
2020-05-19 23:06:10 +1000

    sky

---
+++ Assets/Scripts/Environment/Sky.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sky : MonoBehaviour
+{
+	public float DayLength;
+
+	[SerializeField]
+	private float skySpeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+		skySpeed = Time.deltaTime / DayLength;
+		transform.Rotate(0, skySpeed, 0);
+	}
+}
