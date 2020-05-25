99c4a31e61230b53b04717f702299b76e088ee3b
Peter <pjthompson.au@gmail.com>
2020-05-26 04:13:16 +1000

    Player animation runs at a certain speed due to running/walking/crouching

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,9 +8,36 @@ public class PlayerMovement : MonoBehaviour
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
 	public CharacterController controller;
+
+	public bool isWalking;
+	public bool isRunning;
+	public bool isCrouching;
+	public bool isAirBorn;
+	public bool isAiming;
+
+	public float walkMultiplier = 1.3f;
+	public float runMultiplier = 1.4f;
+	public float crouchMultiplier = 1f;
+	public float airBornMultiplier = 1f;
+
+	public float aimMoveSpeed = 4f;
+
+	public PlayerAnimations pA;
+
 	private Vector3 moveDirection = Vector3.zero;
 
+	private void Start()
+	{
+
+	}
+
 	void Update()
+	{
+		Movements();
+		AnimatorMultipliers();
+	}
+
+	void Movements()
 	{
 		controller = GetComponent<CharacterController>();
 		// is the controller on the ground?
@@ -26,9 +53,14 @@ public class PlayerMovement : MonoBehaviour
 			{
 				moveDirection.y = jumpSpeed;
 			}
+
+			isWalking = true;
+			isRunning = false;
+			isCrouching = false;
+			isAirBorn = false;
 		}
 
-		if (controller.isGrounded && Input.GetButton("Sprint"))
+		if (controller.isGrounded && Input.GetButton("Sprint") && !Input.GetButton("Aim"))
 		{
 			//Feed moveDirection with input.
 			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
@@ -40,34 +72,107 @@ public class PlayerMovement : MonoBehaviour
 			{
 				moveDirection.y = jumpSpeed;
 			}
+			isWalking = false;
+			isRunning = true;
+			isCrouching = false;
+			isAirBorn = false;
 		}
 
 		if (!controller.isGrounded)
 		{
 			//Feed moveDirection with input.
-			moveDirection.x = Input.GetAxis("Horizontal") / 1.2f;
-			moveDirection.z = Input.GetAxis("Vertical") / 1.2f;
+			moveDirection.x = Input.GetAxis("Horizontal") / 1.15f;
+			moveDirection.z = Input.GetAxis("Vertical") / 1.15f;
 			moveDirection = transform.TransformDirection(moveDirection);
 			//Multiply it by speed.
 			moveDirection.x *= speed;
 			moveDirection.z *= speed;
 
+			isWalking = true;
+			isRunning = false;
+			isCrouching = false;
+			isAirBorn = true;
 		}
 
 		if (!controller.isGrounded && Input.GetButton("Sprint"))
 		{
 			//Feed moveDirection with input.
-			moveDirection.x = Input.GetAxis("Horizontal") / 1.2f;
-			moveDirection.z = Input.GetAxis("Vertical") / 1.2f;
+			moveDirection.x = Input.GetAxis("Horizontal") / 1.1f;
+			moveDirection.z = Input.GetAxis("Vertical") / 1.1f;
 			moveDirection = transform.TransformDirection(moveDirection);
 			//Multiply it by speed.
 			moveDirection.x *= runSpeed;
 			moveDirection.z *= runSpeed;
 
+			isWalking = false;
+			isRunning = true;
+			isCrouching = false;
+			isAirBorn = true;
+		}
+
+		if (controller.isGrounded && Input.GetButton("Crouch"))
+		{
+			//Feed moveDirection with input.
+			moveDirection = new Vector3(Input.GetAxis("Horizontal") / 2f, 0, Input.GetAxis("Vertical") / 2f);
+			moveDirection = transform.TransformDirection(moveDirection);
+			//Multiply it by speed.
+			moveDirection *= speed / 2f;
+			//Jumping
+			if (Input.GetButton("Jump"))
+			{
+				moveDirection.y = jumpSpeed;
+			}
+
+			isWalking = false;
+			isRunning = false;
+			isCrouching = true;
+			isAirBorn = false;
+		}
+
+		if (!controller.isGrounded && Input.GetButton("Crouch"))
+		{
+			//Feed moveDirection with input.
+			moveDirection = new Vector3(Input.GetAxis("Horizontal") / 2.25f, 0, Input.GetAxis("Vertical") / 2.25f);
+			moveDirection = transform.TransformDirection(moveDirection);
+			//Multiply it by speed.
+			moveDirection *= speed;
+			//Jumping
+			if (Input.GetButton("Jump"))
+			{
+				moveDirection.y = jumpSpeed;
+			}
+
+			isWalking = false;
+			isRunning = false;
+			isCrouching = true;
+			isAirBorn = true;
 		}
 		//Applying gravity to the controller
 		moveDirection.y -= gravity * Time.deltaTime;
 		//Making the character move
 		controller.Move(moveDirection * Time.deltaTime);
+	}
+
+	void AnimatorMultipliers()
+	{
+		if (pA.playerAnimation.GetCurrentAnimatorStateInfo(0).IsTag("MultiplierChangable"))
+		{
+			if (isWalking)
+			{
+				pA.playerAnimation.speed = walkMultiplier;
+			}
+			if (isRunning)
+			{
+				pA.playerAnimation.speed = runMultiplier;
+			}
+			if (isCrouching)
+			{
+				pA.playerAnimation.speed = crouchMultiplier;
+			}
+			if (isAirBorn)
+			{
+				pA.playerAnimation.speed = airBornMultiplier;
+			}
+		}
 	}
 }
