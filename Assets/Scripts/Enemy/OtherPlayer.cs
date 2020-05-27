d2a8ca1eadf4acc2770bcc037a838af11be67b56
Peter <pjthompson.au@gmail.com>
2020-05-28 04:23:39 +1000

    Added Enemy

---
+++ Assets/Scripts/Enemy/OtherPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherPlayer : MonoBehaviour
+{
+    public GameObject arms;
+    public GameObject lookCam;
+    public GameObject head;
+
+    public float y;
+
+    // Update is called once per frame
+    void Update()
+    {
+        y = 0f + (lookCam.transform.localRotation.x / 10 * 3);
+
+        head.transform.rotation = lookCam.transform.localRotation;
+        if (lookCam.transform.localRotation.x > 0)
+        {
+            arms.transform.localPosition = new Vector3(0.14f, y, -0.075f);
+        }
+    }
+}
