390890a52d43da38609eccc62d16e291fea7c549
Peter <pjthompson.au@gmail.com>
2020-05-31 04:45:30 +1000

    Update CarPlayerCollision.cs

--- Assets/Scripts/Car Mechanics/CarPlayerCollision.cs
+++ Assets/Scripts/Car Mechanics/CarPlayerCollision.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class CarPlayerCollision : MonoBehaviour
 {
-	public float highestVelocity;
-
 	private Rigidbody rigid;
 	
 	// Start is called before the first frame update
@@ -14,26 +12,18 @@ public class CarPlayerCollision : MonoBehaviour
 		rigid = GetComponent<Rigidbody>();
 	}
 
-	// Update is called once per frame
-	void Update()
-	{
-
-	}
-
-	private void FixedUpdate()
-	{
-
-	}
-
 	private void OnCollisionEnter(Collision collision)
 	{
 		if (UIManager.carSpeed > 10f)
 		{
-			if (collision.transform.root.gameObject.tag == "Enemy")
+			if (rigid.mass > 10f)
 			{
-				Enemy enemy = collision.transform.root.GetComponent<Enemy>();
-				enemy.curHealth -= UIManager.carSpeed * rigid.mass;
-			}
+				if (collision.transform.root.gameObject.tag == "Enemy")
+				{
+					Enemy enemy = collision.transform.root.GetComponent<Enemy>();
+					enemy.curHealth -= UIManager.carSpeed * (rigid.mass / 10);
+				}
+			}			
 		}
 	}
 }
