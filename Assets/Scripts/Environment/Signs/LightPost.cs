8d6283762ad82c59eed410f036a70ed4dd9e8e77
Peter <pjthompson.au@gmail.com>
2020-06-02 06:22:01 +1000

    Light Posts

---
+++ Assets/Scripts/Environment/Signs/LightPost.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPost : MonoBehaviour
+{
+    [SerializeField]
+    private Material lightMat;
+
+    private Light spotLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spotLight = GetComponentInChildren<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
