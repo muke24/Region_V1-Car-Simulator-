bd0f784dcc5b6c782455c44b67f50c40d5969409
Peter <pjthompson.au@gmail.com>
2020-06-04 16:19:01 +1000

    Baked navmesh

---
+++ Assets/Scripts/GameModes/Teams.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teams : MonoBehaviour
+{
+    public bool gameMode1v1 = true;
+    public bool gameModeTeams = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
