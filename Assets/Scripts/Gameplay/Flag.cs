1057d03d3ee4e319fad80400b6001f087b6fc6b2
Peter <pjthompson.au@gmail.com>
2020-06-09 01:50:38 +1000

    Some flag stuff

---
+++ Assets/Scripts/Gameplay/Flag.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
+public class Flag : MonoBehaviour
+{
+	private MeshFilter meshFilter;
+
+	public float amplitude = 1f;
+	public float length = 2f;
+	public float speed = 1f;
+	public float offset = 0f;
+
+	// Start is called before the first frame update
+	void Awake()
+	{
+		meshFilter = GetComponent<MeshFilter>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3[] vertices = meshFilter.mesh.vertices;
+		for (int i = 0; i < vertices.Length; i++)
+		{
+			vertices[i].y = GetWaveHeight(transform.position.x + vertices[i].x);
+		}
+
+		meshFilter.mesh.vertices = vertices;
+		meshFilter.mesh.RecalculateNormals();
+
+		offset += Time.deltaTime * speed;
+	}
+
+	public float GetWaveHeight(float _x)
+	{
+		return amplitude * Mathf.Sin(_x / length + offset);
+	}
+}
---
+++ Assets/Scripts/Props/CarObjectCollision.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarObjectCollision : MonoBehaviour
+{
+	private Rigidbody rigid;
+
+	[SerializeField]
+	private Light spotLight = null;
+	[SerializeField]
+	private GameObject halo = null;
+	[SerializeField]
+	private MeshRenderer lightObject = null;
+	[SerializeField]
+	private Material lightOff = null;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rigid = GetComponent<Rigidbody>();
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.rigidbody)
+		{
+			float otherMass = collision.rigidbody.mass;
+
+			var force = collision.relativeVelocity.magnitude * otherMass;
+
+			if (force > 400f)
+			{
+				rigid.isKinematic = false;
+				rigid.AddForce(collision.relativeVelocity * 5, ForceMode.Force);
+				print("Force = " + force);				
+				lightObject.material = lightOff;
+
+				Destroy(spotLight);
+				Destroy(halo);
+				Destroy(this, 0.1f);
+			}
+		}
+	}	
+}
