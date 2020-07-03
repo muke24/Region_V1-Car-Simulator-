fba0a7ea27a20cea64a1340727166a069b5c2f6a
Peter <pjthompson.au@gmail.com>
2020-07-03 23:49:08 +1000

    Messed up buttons and UI in main menu, this should have fixed it

---
+++ Assets/Scripts/MainMenu/DestroyCollidersAtStart.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DestroyCollidersAtStart : MonoBehaviour
+{
+	void Awake()
+	{
+		foreach (Collider col in FindObjectsOfType<Collider>())
+		{
+			if (!col.CompareTag("DontDestroyAtStart"))
+			{
+				Destroy(col);
+			}
+		}
+	}
+}
