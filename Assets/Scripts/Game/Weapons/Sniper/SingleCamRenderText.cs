86580cd72e9961b63a70501bfd29e9a83c9fdf94
Peter <pjthompson.au@gmail.com>
2020-07-03 20:15:37 +1000

    Made building 1 a proper model in blender

---
+++ Assets/Scripts/Game/Weapons/Sniper/SingleCamRenderText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SingleCamRenderText : MonoBehaviour
+{
+	public Camera playerCam;
+	public RenderTexture scopeRenderTexture;
+
+	// Update is called once per frame
+	void Update()
+	{
+		//if (GetComponent<PlayerAnimations>().scoped)
+		//{
+		//	playerCam.targetTexture = scopeRenderTexture;
+
+		//	RenderTexture.active = scopeRenderTexture;
+		//	playerCam.Render();
+
+		//	RenderTexture.active = null;
+		//	playerCam.targetTexture = null;
+		//}
+		//else
+		//{
+		//	RenderTexture.active = null;
+		//	playerCam.targetTexture = null;
+		//}
+	}
+}
