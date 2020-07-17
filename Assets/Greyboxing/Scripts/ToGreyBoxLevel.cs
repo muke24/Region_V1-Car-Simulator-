20671017c68374d64c7b6d53a49c839befda9580
Peter <pjthompson.au@gmail.com>
2020-07-17 22:54:47 +1000

    Greybox Level can be accessed from main menu

---
+++ Assets/Greyboxing/Scripts/ToGreyBoxLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToGreyBoxLevel : MonoBehaviour
+{
+    public void ToGreyboxLevel()
+	{
+		SceneManager.LoadScene(3);
+	}
+}
---
+++ Assets/Greyboxing/Scripts/UI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject pause;
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (pause.activeInHierarchy)
+			{
+				pause.SetActive(false);
+				Cursor.lockState = CursorLockMode.Locked;
+			}
+			else
+			{
+				pause.SetActive(true);
+				Cursor.lockState = CursorLockMode.None;
+			}
+		}
+	}
+
+	public void BackToMainMenu()
+	{
+		SceneManager.LoadScene(0);
+	}
+}
